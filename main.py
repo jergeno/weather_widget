b77455e6b766aa104fee33cbf0d37b90c48733b8
Jeremy Genovese <jeremy.genovese@gmail.com>
2021-03-04 17:20:23 -0500

    Extensive refactoring to use functional blocks. TODO next is to implement printing to the user as a function TODO later is to figure out how to deal if gust does not exist

--- main.py
+++ main.py
@@ -2,29 +2,48 @@
 # Author: Jeremy Genovese
 # Date Created: 3-4-2021
 # Version: 0.1
-#TODO: refactor the code into functional blocks
+#TODO: refactor the code into functional blocks. IN PROGRESS
 
 import json
 import requests
 
 API_KEY_FILE = "/home/hiro/bin/weather_widget/openWeatherMap_API_key"
 API_URL = "http://api.openweathermap.org/data/2.5/weather?id={}&appid={}&units={}"
-CITY_ID = "5014946" # Whitmore Lake
+CITY_ID = {'Ypsilanti' : '5015688', 'Ann Arbor' : '4984247', 'Whitmore Lake' : '5014946', 'Midland' : '5001929'}
 DEFAULT_UNITS = "imperial"
 
 # get the API key
-with open(API_KEY_FILE, "r") as API_file:
-    api_key = API_file.readline()
-    API_file.close()
+def Get_API_key(keyfile):
+    with open(keyfile, "r") as API_file:
+        key = API_file.readline()
+        API_file.close()
+    return key
+
+def Get_city_from_user():
+    print('Select a city from the following options: ')
+    for city in CITY_ID:
+        print(city)
+    selected_city = input()
+    city_num = CITY_ID[selected_city]
+    return city_num
+    
+
+def Get_API_data(key, city_id):
+    response = requests.get(API_URL.format(city_id, key, DEFAULT_UNITS))  # get the data and begin processing
+    weather_text = json.loads(response.text)
+    return weather_text
 
 
-response = requests.get(API_URL.format(CITY_ID, api_key, DEFAULT_UNITS))  # get the data and begin processing
+def main():
+    api_key = Get_API_key(API_KEY_FILE)
+    city_id_num = Get_city_from_user()
+    weather = Get_API_data(api_key, city_id_num)
+    return weather
+weather = main()
 
-weather = json.loads(response.text)
-# # debugging
+
+# debugging
 # print(weather)
-
-# print(weather['main']['temp'])
 
 # store the desired parameters into their own variables
 
@@ -33,7 +52,11 @@ current_temp = weather['main']['temp']
 current_humidity = weather['main']['humidity']
 current_feel = weather['main']['feels_like']
 current_wind_speed = weather['wind']['speed']
-current_wind_gust = weather['wind']['gust']
+# if gust in weather: #TODO: Figure out an elegant way to check for the existence of a key. 'gust' is not always present
+#     current_wind_gust = weather['wind']['gust']
+# else:
+#     pass
 # display the output to the user
+print()
 print("Currently in {}:\nTemperatrure: {} F\nHumidity: {}%\nFeels Like: {} F".format(current_city, current_temp, current_humidity, current_feel))
-print("The wind speed is: {} mph, with gusts to {} mph".format(current_wind_speed, current_wind_gust))
+print("The wind speed is: {} mph".format(current_wind_speed))
