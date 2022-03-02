c36ce6cd83045b90359f5c09ab18cbd569a8f74a
Jeremy Genovese <98819451+jergeno@users.noreply.github.com>
2022-03-01 22:32:32 -0500

    partially working city index retrieval

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+
+
 # A tool to poll Open Weather Map and display the results
 # Author: Jeremy Genovese
 # Date Created: 3-4-2021
@@ -8,6 +11,7 @@ import json
 import requests
 import yaml
 
+cities=[]
 CITY_ID = {'Ypsilanti' : '5015688', 'Ann Arbor' : '4984247', 'Whitmore Lake' : '5014946', 'Midland' : '5001929'}
 
 # load the configuration
@@ -17,13 +21,17 @@ with open("config.yaml") as configFile:
 # get the city id
 with open("city.list.json") as city_list_file:
     cities=json.load(city_list_file)
-    citySubList = []
-    for item in cities:
-        if item["name"] == "Midland":
-            citySubList.append(item)
-    print("Did you mean: ")
-    for item in citySubList:
-        print("{}. {}, {}, {}".format(citySubList.index(item), item["name"], item["state"], item["country"]))
+print(type(cities))
+citySubList = []
+for item in cities:
+    if item["name"] == "Midland":
+        citySubList.append(item)
+print("Did you mean: ")
+for item in citySubList:
+     print("{}. {}, {}, {}".format(citySubList.index(item), item["name"], item["state"], item["country"]))
+city = citySubList[int(input("Enter your choice: "))]
+print(city["id"])
+
         
 # TODO: next steps are to ship printing state if it is empty, and then move on to actually accepting user input ans returning the city id
 
